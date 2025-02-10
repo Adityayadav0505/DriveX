ba4da925d198f17ca88f3080c6ebcaf10290c15b
Aditya <adityayadav213@gmail.com>
2025-02-10 18:39:26 +0530

    H2 Database configured

--- src/main/java/com/drive/file_storage/repository/FileStorageService.java
+++ src/main/java/com/drive/file_storage/repository/FileStorageService.java
@@ -13,18 +13,24 @@ import java.io.IOException;
 public class FileStorageService {
 
     private final AmazonS3 s3Client;
+    private FileService fileService;
 
     @Value("${aws.s3.bucket-name}")
     private String bucketName;
 
-    public FileStorageService(AmazonS3 s3Client) {
+    public FileStorageService(AmazonS3 s3Client, FileService fileService) {
         this.s3Client = s3Client;
+        this.fileService = fileService;
     }
 
     public String uploadFile(MultipartFile file) throws IOException {
         String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
         s3Client.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), null));
-        return s3Client.getUrl(bucketName, fileName).toString(); // Returning the file URL
+        String fileUrl = s3Client.getUrl(bucketName, fileName).toString(); // Returning the file URL
+
+        fileService.saveFileMetadata(fileName, fileUrl);
+
+        return fileUrl;
     }
 
     public S3Object downloadFile(String fileName) {
